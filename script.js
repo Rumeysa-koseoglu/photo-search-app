d0478e17c5e84bed85b9e488e1ff156e50e3411a
Rumeysa Koseoglu <koseoglurumeysa0@gmail.com>
2025-04-06 13:45:08 +0300

    feat: add initial JavaScript references

---
+++ script.js
@@ -0,0 +1,7 @@
+const formwrapper = document.querySelector(".form-wrapper");
+const form = document.querySelector("#form");
+const searchInput = document.querySelector("#searchInput");
+const buttonWrapper = document.querySelector(".button-wrapper");
+const searchButton = document.querySelector("#searchButton");
+const clearButton = document.querySelector("#clearButton");
+const imageListWrapper = document.querySelector(".imageList-wrapper");
